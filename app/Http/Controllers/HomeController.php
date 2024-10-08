598c361c1fd04d0f2115b30feb310cd6b2c2b4ee
devran.demir <secondply.2@gmail.com>
2024-10-08 13:06:23 +0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $users = User::count();
+
+        $widget = [
+            'users' => $users,
+            //...
+        ];
+
+        return view('/', compact('widget'));
+    }
+}
