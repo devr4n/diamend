60bd7a763541870b4106a2c3d5cdb8d3febc9eae
devr4n <secondply.2@gmail.com>
2024-12-14 18:58:13 +0200

    #7 expenses lang update

---
+++ resources/lang/en/expenses.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'id' => 'ID',
+    'expenses' => 'Expenses',
+    'expense' => 'Expense',
+    'expense_type' => 'Expense Type',
+    'expense_types' => 'Expense Types',
+    'amount' => 'Amount',
+    'note' => 'Note',
+    'date' => 'Date',
+    'action' => 'Action',
+];
---
+++ resources/lang/tr/expenses.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'id' => 'ID',
+    'expenses' => 'Giderler',
+    'expense' => 'Gider',
+    'expense_type' => 'Gider Türü',
+    'expense_types' => 'Gider Türleri',
+    'amount' => 'Miktar',
+    'note' => 'Not',
+    'date' => 'Tarih',
+    'action' => 'İşlem',
+];
